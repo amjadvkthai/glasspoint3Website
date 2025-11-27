03c9bf32c435acf55c36dfb489b6aa4167e0c1d9
Amjad V K <amjadvk@teamthai.in>
2025-11-27 10:34:33 +0530

    ok

--- constants.ts
+++ constants.ts
@@ -18,7 +18,7 @@ export const SERVICES: ServiceItem[] = [
   { 
     id: 1, 
     title: 'Precision Machine Beveling', 
-    image: 'https://images.unsplash.com/photo-1590499099307-883398c8c2c1?q=80&w=1200&auto=format&fit=crop', 
+    image: 'https://raw.githubusercontent.com/amjadvkthai/glasspoint3Website/refs/heads/main/assets/image1.png', 
     size: 'large' 
   },
   { 
@@ -30,7 +30,7 @@ export const SERVICES: ServiceItem[] = [
   { 
     id: 3, 
     title: 'Glass Polishing', 
-    image: 'https://images.unsplash.com/photo-1628144211608-d227d896435d?q=80&w=800&auto=format&fit=crop', 
+    image: 'https://raw.githubusercontent.com/amjadvkthai/glasspoint3Website/refs/heads/main/assets/image1.png', 
     size: 'standard' 
   },
   { 
@@ -42,7 +42,7 @@ export const SERVICES: ServiceItem[] = [
   { 
     id: 5, 
     title: 'Acid Work & Texturing', 
-    image: 'https://images.unsplash.com/photo-1534353875273-b5887cc17a5b?q=80&w=800&auto=format&fit=crop', 
+    image: 'https://raw.githubusercontent.com/amjadvkthai/glasspoint3Website/refs/heads/main/assets/image1.png', 
     size: 'standard' 
   },
   { 
